f19659e0b9098f07cd3e369c92fde37544c9f17e
Daniel Yudi <peo_dushino@uolinc.com>
2015-07-06 17:16:53 -0300

    Commit inicial do projeto]

---
+++ marte/src/main/java/com/elo7/marte/domain/LatVO.java
@@ -0,0 +1,24 @@
+package com.elo7.marte.domain;
+
+public class LatVO {
+
+	private int value;
+
+	public LatVO(int value) {
+		super();
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+	
+	public int plus() {
+		return ++value;
+	}
+	
+	public int minus() {
+		return --value;
+	}
+
+}
